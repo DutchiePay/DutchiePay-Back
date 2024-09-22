a402527e417962fc29e55b1f9c8e4a1507fbd985
dnjsals45 <dnjsals45@naver.com>
2024-09-22 18:48:48 +0900

    feat | 나눔게시글, 자유게시글 생성 응답 dto 작성

---
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/community/dto/CreateSharePostResponseDto.java
@@ -0,0 +1,11 @@
+package dutchiepay.backend.domain.community.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateSharePostResponseDto {
+    private Long sharePostId;
+}
